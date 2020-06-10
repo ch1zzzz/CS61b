15d9e29f98596b6401e46c933941b6c0d8d3f754
Ch1zzZ <694993696@qq.com>
2020-06-10 23:16:39 +0800

    complete lab7 with option exercise

--- lab7/BSTMap.java
+++ lab7/BSTMap.java
@@ -1,7 +1,9 @@
 import com.sun.jdi.Value;
 
 import java.security.Key;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
@@ -38,7 +40,10 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
     /* Returns the value to which the specified key is mapped, or null if this
      * map contains no mapping for the key.
      */
-
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    };
     private V get(Node node, K key) {
         if (key == null) throw new IllegalArgumentException("calls get() with a null key");
         if (node == null) {return null;}
@@ -47,12 +52,6 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
         else if (cmp > 0) {return get(node.right, key);}
         else return node.value;
     }
-
-
-    @Override
-    public V get(K key) {
-        return get(root, key);
-    };
 
     /* Returns the number of key-value mappings in this map. */
     @Override
@@ -85,7 +84,11 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException("Unsupported operation");
+        Set<K> KeySet = new HashSet<>();
+        for (int i = 0; i < size(); i += 1) {
+            KeySet.add(select(i).key);
+        }
+        return KeySet;
     };
 
     /* Removes the mapping for the specified key from this map if present.
@@ -93,20 +96,95 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException("Unsupported operation");
+        if (key == null) throw new IllegalArgumentException();
+        return remove_helper(key, null);
     };
+
 
     /* Removes the entry for the specified key only if it is currently mapped to
      * the specified value. Not required for Lab 8. If you don't implement this,
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException("Unsupported operation");
+        if (key == null) throw new IllegalArgumentException();
+        if (value == null) throw new IllegalArgumentException();
+        return remove_helper(key, value);
     };
 
+    private V remove_helper(K key, V value) {
+        V tar_value = get(key);
+        if (tar_value == null) {return null;} /* key not found. */
+        if (value != null && !tar_value.equals(value)) {return null;}
+        root = delete(root, key);
+        return tar_value;
+    }
+
+    public void delete(K key) {
+        if (key == null) throw new IllegalArgumentException("calls delete() with a null key");
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, K key) {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left  = delete(x.left,  key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left  == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public K min() {
+        if (size() == 0) throw new NoSuchElementException("calls min() with empty symbol table");
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
+
+    public void deleteMin() {
+        if (size() == 0) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    /** Don't know how to write iterator. */
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return new BSTMapIterator();
+    }
+
+    private class BSTMapIterator implements Iterator<K> {
+        private int pos;
+        public BSTMapIterator() {
+            pos = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            return pos < size();
+        }
+        @Override
+        public K next() {
+            K key = select(pos).key;
+            pos += 1;
+            return key;
+        }
     }
 
     public void printInOrder() {
@@ -142,10 +220,13 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
         }
     }
 
+
     public static void main(String[] args) {
         BSTMap<String, Integer> b = new BSTMap<String, Integer>();
-        int i = 0;
-        b.put("hi" + i, 1+i);
-        System.out.println(b.get("hi" + i));
+        for (int i = 0; i < 50; i++) {
+            b.put("hi" + i, 1 + i);}
+        for (String s : b) {
+            System.out.println(s);
+        }
     }
 }
